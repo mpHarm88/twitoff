afad1a1647adc703f42e62edeaac7cd6805f9317
mpHarm88 <mpHarm88@users.noreply.github.com>
2019-11-12 14:47:36 -0800

    edited app.py and models.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from .models import DB
-
+from flask import Flask, render_template, request
+from .models import DB, User, Tweet
+from decouple import config
 
 # now we make a app factory
 def create_app():
@@ -8,11 +8,22 @@ def create_app():
     
     # add our config
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     # now have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        tweets = Tweet.query.all()
+        return render_template('base.html', title='Home', users=users, tweets=tweets)
     return app
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset', users=[])
+    return app 
+
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,26 +1,29 @@
-""""These are my database models  """
+"""These are my database models"""
 
 from flask_sqlalchemy import SQLAlchemy
 
-# import database for global scope
+# import database. capital for global scope
 DB = SQLAlchemy()
 
 
 class User(DB.Model):
-    """Twitter users we analyze  """
-    id = DB.Column(DB.Integer, primary_key=True)
+    """Twitter users that we analyze"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
-    newest_tweet_id = DB.Column(DB.BigInteger) 
+    newest_tweet_id = DB.Column(DB.BigInteger)
+
     def __repr__(self):
-        return '<User {}>.format(self.name)'
+        return '<User {}>'.format(self.name) 
 
 
 class Tweet(DB.Model):
-    """The users tweets from twitter"""
-    id = DB.Column(DB.BigInteger, primary_key=True)
+    """The user's tweets from twitter"""
+    id = DB.Column(DB.Integer, primary_key=True)
     text = DB.Column(DB.Unicode(300))
-    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    embedding = DB.Column(DB.PickleType, nullable=False)
 
     def __repr__(self):
         return '<Tweet {}>'.format(self.text)
